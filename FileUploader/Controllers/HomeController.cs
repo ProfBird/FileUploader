6828500ed2ee8ae58547e47e70794099d43d6778
Brian Bird <ProfBird@users.noreply.github.com>
2022-03-10 17:05:36 -0800

    Add project files.

---
+++ FileUploader/Controllers/HomeController.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        /************* File Upload Action Methods ***********/
+
+        private readonly long _fileSizeLimit = 2097162; // 2,097,162
+        private readonly string[] _permittedExtensions = { ".txt", ".csv" };
+        private readonly string _targetFilePath = "\\files";
+        public string Result { get; private set; }
+
+        [HttpGet]
+        public IActionResult Upload()
+        {
+            BufferedSingleFileUploadPhysical model = new();
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Upload(IFormFile formFile)
+        {
+            if (!ModelState.IsValid)
+            {
+                Result = "Please correct the form.";
+
+                return View();
+            }
+
+            var formFileContent =
+                await FileHelpers.ProcessFormFile<BufferedSingleFileUploadPhysical>(
+                    formFile, ModelState, _permittedExtensions,
+                    _fileSizeLimit);
+
+            if (!ModelState.IsValid)
+            {
+                Result = "Please correct the form.";
+                return View();
+            }
+
+            // For the file name of the uploaded file stored
+            // server-side, use Path.GetRandomFileName to generate a safe
+            // random file name.
+            var trustedFileNameForFileStorage = Path.GetRandomFileName();
+            var filePath = Path.Combine(
+                _targetFilePath, trustedFileNameForFileStorage);
+
+            // **WARNING!**
+            // In the following example, the file is saved without
+            // scanning the file's contents. In most production
+            // scenarios, an anti-virus/anti-malware scanner API
+            // is used on the file before making the file available
+            // for download or for use by other systems. 
+            // For more information, see the topic that accompanies 
+            // this sample.
+
+            using (var fileStream = System.IO.File.Create(filePath))
+            {
+                await fileStream.WriteAsync(formFileContent);
+
+                // To work directly with a FormFile, use the following
+                // instead:
+                //await FileUpload.FormFile.CopyToAsync(fileStream);
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ FileUploader/Models/BufferedSingleFileUploadPhysical.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication1.Models
+{
+    public class BufferedSingleFileUploadPhysical
+    {
+        public IFormFile FormFile { get; set; }
+
+        public string Note { get; set; }
+    }
+}
